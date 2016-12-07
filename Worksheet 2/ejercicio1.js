475c65cb46ec7c9637cd236a91d133399611832e
Fran Saavedra <fran14.95@gmail.com>
2016-12-07 08:32:39 +0100

    Añadidos bloques al Arkanoid

--- Worksheet 2/ejercicio1.js
+++ Worksheet 2/ejercicio1.js
@@ -6,12 +6,25 @@ class Juego{
         zona_juego.style.border = "solid black";
         document.body.appendChild(zona_juego);
 
-        var x = Math.round(Math.random()*480) + 16;
-        var bola = new Bola(x,16,1,1,15,"red");
+        var x = Math.round(Math.random()*470) + 16;
+        var bola = new Bola(x,140,1,1,15,"red");
         bola.move();
         zona_juego.appendChild(bola.bola);
 
         zona_juego.appendChild(base.base);
+        var posX = 5;
+        var posY = 0;
+        var id = 0;
+        for (var i = 1; i < 6; i++) {
+            for (var j = 1; j < 6; j++) {
+                var bloque = new Base(posX,posY,20,90,"blue",id++);
+                zona_juego.appendChild(bloque.base);
+                arrayBloques.push(bloque);
+                posX += 100;
+            }
+            posX = 5;
+            posY += 25;
+        }
     }
 
 }
@@ -40,7 +53,7 @@ class Bola{
 
             if (self.ifEndGame(self.bola.cy.baseVal.value,self.r,zona_juego.height.baseVal.value)) {
                 clearInterval(intervalo);
-                alert("Has perdido");
+               
             }else{
                 if (self.ifVerticalCollision(self.bola.cy.baseVal.value,self.r,zona_juego.height.baseVal.value)) {
                     self.cy *= -1;
@@ -51,8 +64,17 @@ class Bola{
                 }
 
                 if (self.ifBaseCollision(self.bola.cy.baseVal.value,self.r,base.y,self.bola.cx.baseVal.value,base.x,base.width)) {
-                        //self.cx *= -1;
                         self.cy *= -1;
+                }
+
+                for (var i = 0; i < arrayBloques.length; i++) {
+                    if (zona_juego.children.namedItem(i).style.display != "none") {
+                        if (self.ifBloqueCollision(self.bola.cy.baseVal.value,self.r,arrayBloques[i].y,self.bola.cx.baseVal.value,
+                            arrayBloques[i].x,arrayBloques[i].width,arrayBloques[i].height)) {
+                            zona_juego.children.namedItem(i).style.display = "none";
+                            self.cy *= -1;
+                        }
+                    }
                 }
                 
                 self.x += self.cx;
@@ -79,20 +101,29 @@ class Bola{
     }
 
     ifBaseCollision(cyBola,rBola,cyBase,cxBola,cxBase,wBase){
-        if (cyBola >=(cyBase - rBola) && (cxBola >=(cxBase - rBola) && cxBola <=((cxBase + wBase)- rBola)))  
+        //posicion Y de bola >= (posicion Y de base - radio de bola)
+        if ((cyBola >=(cyBase - rBola)) && (cxBola >=(cxBase - rBola) && cxBola <=((cxBase + wBase)- rBola)))  
+            return true;
+        return false;
+    }
+
+    ifBloqueCollision(cyBola,rBola,cyBase,cxBola,cxBase,wBase,hBase){
+        if ((cyBola >=(cyBase + rBola) && cyBola <=((cyBase + hBase)+ rBola)) &&
+         (cxBola >=(cxBase + rBola) && cxBola <=((cxBase + wBase)+ rBola)))  
             return true;
         return false;
     }
 
 }
 
-class Base{
-    constructor(x,y,height,width,color){
+class Bloque{
+    constructor(x,y,height,width,color,id){
         this.x = x;
         this.y = y;
         this.height = height;
         this.width = width;
         this.color = color;
+        this.id = id;
 
         this.base = document.createElementNS("http://www.w3.org/2000/svg","rect");
         this.base.setAttribute("x", this.x);
@@ -100,7 +131,11 @@ class Base{
         this.base.setAttribute("height", this.height);
         this.base.setAttribute("width", this.width);
         this.base.setAttribute("fill", this.color);
+        this.base.setAttribute("id", this.id);
     }
+}
+
+class Base extends Bloque{
 
     moveLeft(){
         this.x -=10;
@@ -117,14 +152,17 @@ class Base{
 window.onload = function () {
     zona_juego = document.createElementNS("http://www.w3.org/2000/svg","svg");
     base = new Base(200,470,20,100,"black");
+    arrayBloques = [];
 
     document.addEventListener("keydown", function (e){
         switch (e.keyCode) {
             case 37:
-                base.moveLeft();
+                if(base.x >= 10)
+                    base.moveLeft();
                 break;
             case 39:
-                base.moveRight();
+                if(base.x < 400)
+                    base.moveRight();
                 break;
             default:
                 // statements_def
